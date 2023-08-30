b7a76650ce26bc83b4efe465cad489b789fa7d33
Shajee-j <Shajeejm@gmail.com>
2023-08-30 04:46:43 -0400

    implement admin dashboard contents

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin
 
+    def initialize
+      @product_count = Product.count
+      @category_count = Category.count
+    end
+  end
+
   def show
   end
 
@@ -9,7 +15,6 @@ class Admin::DashboardController < ApplicationController
   def authenticate_admin
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
-    end
   end
   
 end
